0b108e3d0af36305ad165d9d7556531d81a93beb
Naumov Ilya <naumovilya99@mail.ru>
2021-02-22 12:37:18 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+	$greeting = 'Добро пожаловать на мою страницу!';
+
+	$name = 'Илья';
+	$surname = 'Наумов';
+	$city = 'Москва';
+	$age = 21;
+
+	$phpLogo = 'img/php-logo.svg';
+	$myPhoto = 'img/myPhoto.png';
+?>
